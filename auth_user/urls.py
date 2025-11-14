759fee98b90b98b843b029510546c042e247baf6
Kadi Aziev <kadina_job@mail.ru>
2025-11-14 18:52:59 +0300

    new reg

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from auth_user import views
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('user_profile/<int:pk>/', views.detail_view, name='user_profiile'),
+    # path('user_update/<int:pk>/', views.ProfileUpdateView.as_view()) 
 ]
--- auth_user/views.py
+++ auth_user/views.py
@@ -1,25 +1,43 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView 
+from django.views.decorators.csrf import csrf_exempt
 
 from auth_user.models import User 
 from auth_user.forms import RegisterForm
 
 
 import pdb 
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid() and form.cleaned_data['password'] == form.cleaned_data['password_confirmation']:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_conf = request.POST.get('password_confirmation')
+        if password == password_conf:
             user = User.objects.create(
-                name=form.cleaned_data['name'],
-                email=form.cleaned_data['email'],
-                password=form.cleaned_data['password'] 
+                name=name,
+                email=email,
+                password=password
             )
-            return HttpResponseRedirect('/profile')
+            return HttpResponseRedirect(f"/user_profiile/{user.pk}/")
+    return render(request, 'auth_user/register_form.html')
+
+
+def detail_view(request, pk):
+    user = User.objects.get(pk=pk)
+    if not user.is_active:
+        raise User.DoesNotExist('User matching query does not exist')
 
     context = {
-        'form': RegisterForm(),
+        'user': user
     }
-    return render(request, 'auth_user/register.html', context=context)
+    
+    return render(request, 'auth_user/user_detail.html', context=context)
+    
 
+def update_profile(request):
+    pass
 
